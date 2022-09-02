ff4fcb694c5ad4cf41582a5b7e082235cce3a380
unknown <tohami00076@gmail.com>
2022-09-02 10:24:31 -0700

    jdfhvzd

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <my-form></my-form>
+</template>
+
+<script >
+
+
+import myForm from './components/myForm.vue'
+
+
+    export default {
+    components : { myForm},
+    }
+
+
+</script>
+
+
+<style >
+
+</style>
---
+++ src/components/myForm.vue
@@ -0,0 +1,47 @@
+<template>
+    {{name}}
+    {{age}}
+    <br>
+    {{hopiets[0]}}
+    <br>
+
+<!----------------------------------------------    directive in vue -->
+    <h2 v-text="name"></h2>
+    <br>
+
+
+    <div v-if="hopiets.length > 5 ">
+        {{hopiets}}
+    </div>
+
+    <div v-show="hopiets.length > 0 ">
+        {{hopiets}}
+    </div>
+
+
+
+
+
+
+</template>
+
+
+<script>
+export  default{
+    data() {
+
+        return{
+            name:'toham',
+            age :28,
+            hopiets : ['play','programing','drawing']
+        }
+
+    }
+}
+
+</script>
+
+
+<style>
+
+</style>
